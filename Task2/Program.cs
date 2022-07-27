aa7ec591a4070be1f2c3554c72b273f94cf3e789
Maksim.Dumkin <dumkin_m@rambler.ru>
2022-07-27 11:53:41 +0300

    Create task2

---
+++ Task2/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 2: Задайте массив строк. Напишите программу, которая генерирует новый массив, 
+//объединяя элементы исходного массива попарно.
+
+//Пример: { "qwe", "wer", "ert", "rty", "tyu", "yui"} -> { "qwewer", "ertrty", "tyuyui"}
+string[] FillVowelLetter(string[]  word)
+{	
+    string[] change=new string[word.Length/2];
+	for (int i=0; i<word.Length-1; i++)
+        {
+        word[i]=word[i]+word[i+1];
+        }
+        
+	return word;
+}
+
+
+
+void    ShowWords(string[] words)
+{
+	for (int  i=0;i<words.Length;i++)
+		Console.Write(words[i]+"  ");
+}
+
+void    ShowWordsCheng(string[] words)
+{
+	for (int  i=0;i<words.Length;i+=2)
+		Console.Write(words[i]+"  ");
+}
+string[] wordsarray={"wer","opera","soft","aeroport","elephant","qwe", "wer", "ert", "rty", "tyu"};
+
+ShowWords(wordsarray);
+
+Console.WriteLine();
+
+ShowWordsCheng(FillVowelLetter(wordsarray));
